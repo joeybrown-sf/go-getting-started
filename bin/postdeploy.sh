a195fe3838447fa2cbb2bf66e879f9403b624b9b
Joey Brown <brown.joseph@salesforce.com>
2024-04-01 16:28:48 -0500

    add set -x

--- bin/postdeploy.sh
+++ bin/postdeploy.sh
@@ -1,8 +1,21 @@
 # Path: bin/predeploy.sh
 #!/bin/sh
 
+set -x
+
+echo "Starting task 1"
+while true; do
+  echo "Resource monitor: $(date)"
+  /bin/sleep 2
+done &
+task1_pid=$!
+echo "Task 1 PID: $task1_pid"
+
 echo "Starting task 2"
-for i in {0..60}; do
-  echo "Long running task: $i/60"
-  sleep 1
+for i in {0..5}; do
+  echo "Long running task: $i/5"
+  /bin/sleep 3
 done
+
+kill $task1_pid
+exit 0
