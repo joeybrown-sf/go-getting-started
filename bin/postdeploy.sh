96ae9644c2e66aa6ef763d1b4049da0da5e66aec
Joey Brown <brown.joseph@salesforce.com>
2024-04-01 16:17:53 -0500

    hmmm

--- bin/postdeploy.sh
+++ bin/postdeploy.sh
@@ -1,20 +1,8 @@
 # Path: bin/predeploy.sh
 #!/bin/sh
 
-echo "Starting task 1"
-while true; do
-  echo "Resource monitor: $(date)"
-  sleep 2
-done &
-task1_pid=$!
-echo "Task 1 PID: $task1_pid"
-
 echo "Starting task 2"
-# do a for loop through 0-5 and output each number. sleep for 3 seconds.
-for i in {0..5}; do
-  echo "Long running task: $i/5"
-  sleep 3
+for i in {0..60}; do
+  echo "Long running task: $i/60"
+  sleep 1
 done
-
-kill $task1_pid
-exit 0
